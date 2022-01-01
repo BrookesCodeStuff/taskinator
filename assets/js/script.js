aa8cb8cf352be0c4514c0561e82266b789029bfc
Brooke <brookepaglia@gmail.com>
2021-12-31 22:03:26 -0500

    update task add to use form submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,20 @@
-let taskBtn = document.querySelector('#save-task');
+// Variables
+let formEl = document.querySelector('#task-form');
 let tasksToDoEl = document.querySelector('#tasks-to-do');
 
-let handleCreateTask = function () {
+// Functions
+// Add a task to the list
+let handleCreateTask = function (event) {
+  // Stop the form from submitting and refreshing the page
+  event.preventDefault();
+
+  // Add list item with text and append it to task list ul
   let taskItemEl = document.createElement('li');
   taskItemEl.className = 'task-item';
   taskItemEl.textContent = 'This is a new task';
   tasksToDoEl.appendChild(taskItemEl);
 };
 
-taskBtn.addEventListener('click', handleCreateTask);
+// Event Listeners
+// When 'Add Task' button is clicked, run create task function
+formEl.addEventListener('submit', handleCreateTask);
