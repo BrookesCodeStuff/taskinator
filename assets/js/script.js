ef067aa4fe84afb38374b81494548f056b33e57c
Brooke <brookepaglia@gmail.com>
2021-12-31 22:37:34 -0500

    accept user provide new task text and type option

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,27 @@ let handleCreateTask = function (event) {
   // Stop the form from submitting and refreshing the page
   event.preventDefault();
 
-  // Add list item with text and append it to task list ul
-  let taskItemEl = document.createElement('li');
-  taskItemEl.className = 'task-item';
-  taskItemEl.textContent = 'This is a new task';
-  tasksToDoEl.appendChild(taskItemEl);
+  // Get the text that was entered in the input and the option from the dropdown
+  let taskNameInput = document.querySelector("input[name='task-name']").value;
+  let taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+  // Create the list item
+  let listItemEl = document.createElement('li');
+  listItemEl.className = 'task-item';
+
+  // Create div to hold task info and add it to list item
+  let taskInfoEl = document.createElement('div');
+  taskInfoEl.className = 'task-info';
+
+  // Add HTML content to div
+  taskInfoEl.innerHTML = `
+  <h3 class='task-name'>${taskNameInput}</h3>
+  <span class='task-type'>${taskTypeInput}</span>`;
+
+  listItemEl.appendChild(taskInfoEl);
+
+  // Add entire list item to list
+  tasksToDoEl.appendChild(listItemEl);
 };
 
 // Event Listeners
